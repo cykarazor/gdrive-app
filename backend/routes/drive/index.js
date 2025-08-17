5c44dd5e91dfc5d623b1763cc4e1ba2018a53b19
Shawn Mohammed <shawn_mohammed@hotmail.com>
2025-08-17 03:34:42 -0400

    Put back drive.js

--- backend/routes/drive/index.js
+++ backend/routes/drive/index.js
@@ -1,8 +1,7 @@
 // backend/routes/drive/index.js
 const express = require('express');
-const driveSvc = require('../../services/drive'); // your new drive service
 
-// Import sub-route modules
+// Import sub-routes
 const testRoute = require('./testRoute');
 const uploadFile = require('./uploadFile');
 const listFiles = require('./listFiles');
@@ -12,16 +11,18 @@ const moveFile = require('./moveFile');
 const getFileMetadata = require('./getFileMetadata');
 const downloadFile = require('./downloadFile');
 
-const router = express.Router();
+module.exports = (driveSvc) => {
+  const router = express.Router();
 
-// Mount sub-routes, passing driveSvc
-router.use('/test', testRoute(driveSvc));
-router.use('/upload', uploadFile(driveSvc));
-router.use('/files', listFiles(driveSvc));
-router.use('/folder', createFolder(driveSvc));
-router.use('/file/:id/delete', deleteFile(driveSvc));
-router.use('/file/:id/move', moveFile(driveSvc));
-router.use('/file/:id/metadata', getFileMetadata(driveSvc));
-router.use('/file/:id/download', downloadFile(driveSvc));
+  // Mount sub-routes, passing in driveSvc
+  router.use('/test', testRoute(driveSvc));
+  router.use('/upload', uploadFile(driveSvc));
+  router.use('/files', listFiles(driveSvc));
+  router.use('/folder', createFolder(driveSvc));
+  router.use('/file/:id/delete', deleteFile(driveSvc));
+  router.use('/file/:id/move', moveFile(driveSvc));
+  router.use('/file/:id/metadata', getFileMetadata(driveSvc));
+  router.use('/file/:id/download', downloadFile(driveSvc));
 
-module.exports = router;
+  return router;
+};
--- backend/services/drive.js
+++
@@ -1,7 +0,0 @@
-// backend/services/drive.js
-const createDriveService = require('./googleDriveService');
-const authClient = require('../config/googleAuth'); // or wherever your OAuth client is
-
-const driveSvc = createDriveService(authClient);
-
-module.exports = driveSvc;
