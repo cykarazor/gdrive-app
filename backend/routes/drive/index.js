4107e40ed25a69c26cef710d101e7ad6dac08d48
Shawn Mohammed <shawn_mohammed@hotmail.com>
2025-08-17 03:01:21 -0400

    Rewrite all root files

--- backend/routes/drive/index.js
+++ backend/routes/drive/index.js
@@ -1,29 +1,28 @@
 // backend/routes/drive/index.js
 const express = require('express');
-const createDriveService = require('../../services/googleDriveService');
-const authClient = require('./_authClient');
 
-const router = express.Router();
-const driveSvc = createDriveService(authClient);
+// Import sub-routes
+const testRoute = require('./testRoute');
+const uploadFile = require('./uploadFile');
+const listFiles = require('./listFiles');
+const createFolder = require('./createFolder');
+const deleteFile = require('./deleteFile');
+const moveFile = require('./moveFile');
+const getFileMetadata = require('./getFileMetadata');
+const downloadFile = require('./downloadFile');
 
-// Import route modules
-const testRoute = require('./testRoute')(driveSvc);
-const uploadFile = require('./uploadFile')(driveSvc);
-const listFiles = require('./listFiles')(driveSvc);
-const createFolder = require('./createFolder')(driveSvc);
-const deleteFile = require('./deleteFile')(driveSvc);
-const moveFile = require('./moveFile')(driveSvc);
-const getFileMetadata = require('./getFileMetadata')(driveSvc);
-const downloadFile = require('./downloadFile')(driveSvc);
+module.exports = (driveSvc) => {
+  const router = express.Router();
 
-// Mount route modules
-router.use('/test', testRoute);
-router.use('/upload', uploadFile);
-router.use('/files', listFiles);
-router.use('/folder', createFolder);
-router.use('/file/:id/delete', deleteFile);
-router.use('/file/:id/move', moveFile);
-router.use('/file/:id/metadata', getFileMetadata);
-router.use('/file/:id/download', downloadFile);
+  // Mount sub-routes, passing in driveSvc
+  router.use('/test', testRoute(driveSvc));
+  router.use('/upload', uploadFile(driveSvc));
+  router.use('/files', listFiles(driveSvc));
+  router.use('/folder', createFolder(driveSvc));
+  router.use('/file/:id/delete', deleteFile(driveSvc));
+  router.use('/file/:id/move', moveFile(driveSvc));
+  router.use('/file/:id/metadata', getFileMetadata(driveSvc));
+  router.use('/file/:id/download', downloadFile(driveSvc));
 
-module.exports = router;
+  return router;
+};
